fe66c857078768b789b4fc7c1548b5176be9a53f
oli <oliver.stadie@gmail.com>
2014-06-22 04:16:53 +0200

    added temorary debug code.

--- app/app-war/src/main/webapp/js/store.js
+++ app/app-war/src/main/webapp/js/store.js
@@ -1,44 +1,60 @@
 var Store = {
 
-    // REST end point
-    endpointUpc: '/api/upc',
+	// cache and compile template
+	errorTemplate : $("#error"),
 
-    callbackUrl : "http://" + window.location.host +  "/html/store/index.html?upc={CODE}",
+	// REST end point
+	endpointUpc : '/api/upc',
 
-    init: function () {
+	callbackUrl : "http://" + window.location.host
+			+ "/html/store/index.html?upc={CODE}",
 
-        $("#newPaymentOrderForm").submit(function (e) {
-            e.preventDefault();
-            $('#qrcode').html("");
-            $('#qrcode').qrcode($(this).serialize());
-            $('#qrCodePanel').slideDown();
+	init : function() {
 
-        });
+		$("#newPaymentOrderForm").submit(function(e) {
+			e.preventDefault();
+			$('#qrcode').html("");
+			$('#qrcode').qrcode($(this).serialize());
+			$('#qrCodePanel').slideDown();
 
-        $("#scan").attr('href', Store.getScanUrl());
-        Store.loadUpc();
-    },
+		});
 
-    loadUpc : function(){
-        var query = Helpers.parseQuery(window.location.search.substring(1));
-        if(!query.upc){
-            return;
-        }
+		$("#scan").attr('href', Store.getScanUrl());
+		Store.loadUpc();
+	},
 
-        $.getJSON(Store.endpointUpc, {upc:query.upc}, function(data){
-            var product = data[0];
-            if(product){
-                $("#name").val(decodeURIComponent(product.productname));
-                $("#img").attr('src',decodeURIComponent(product.imageurl));
-                $("#imgurl").val(decodeURIComponent(product.imageurl));
-                $("#img").show();
-            }
-        });
+	loadUpc : function() {
+		var query = Helpers.parseQuery(window.location.search.substring(1));
+		if (!query.upc) {
+			return;
+		}
 
-    },
+		$.getJSON(Store.endpointUpc, {
+			upc : query.upc
+		}, function(data) {
+			var product = data[0];
+			if (product) {
+				$("#name").val(decodeURIComponent(product.productname));
+				$("#img").attr('src', decodeURIComponent(product.imageurl));
+				$("#imgurl").val(decodeURIComponent(product.imageurl));
+				$("#img").show();
+			}
 
-    getScanUrl: function(){
-        return  "http://zxing.appspot.com/scan?ret=" + encodeURIComponent(Store.callbackUrl);
-    }
+			Store.errorTemplate.children('span').html(
+					'productname: "' + product.productname + '"<br />'
+							+ 'imageurl: "' + product.imageurl + '"<br />'
+							+ 'decodeURIComponent(product.productname): "'
+							+ decodeURIComponent(product.productname)
+							+ '"<br />'
+							+ 'decodeURIComponent(product.imageurl): "'
+							+ decodeURIComponent(product.imageurl) + '"<br />');
+			Store.errorTemplate.show();
+		});
+
+	},
+
+	getScanUrl : function() {
+		return "http://zxing.appspot.com/scan?ret="
+				+ encodeURIComponent(Store.callbackUrl);
+	}
 };
-
